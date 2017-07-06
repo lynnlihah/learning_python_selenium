7efc8b1835119bfcbd67a4805022e8161893531b
lizhufeng <lizhufeng@zdns.cn>
2017-07-06 17:26:01 +0800

    add some files

---
+++ handle_browser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+url = "http://www.baidu.com"
+
+print("now access %s" % url)
+browser.get(url)
+
+time.sleep(2)
+
+# 将浏览器最大化 - 报错
+# browser.maximize_window()
+# time.sleep(3)
+
+# 设置浏览器固定宽高 - 报错
+# browser.set_window_size(480, 800)
+# time.sleep(3)
+
+# 前进后退
+s_url = "http://news.baidu.com"
+browser.get(s_url)
+time.sleep(2)
+
+browser.back()
+time.sleep(1)
+
+browser.forward()
+time.sleep(2)
+
+browser.quit()
--- setup_test.py
+++ setup_test.py
@@ -1,8 +1,15 @@
 # coding = utf-8
 
 from selenium import webdriver
+import time
 
-driver = webdriver.Chrome()
-driver.get('http://www.baidu.com')
-print(driver.title)
-driver.quit()
+# browser = webdriver.Firefox() - 报错
+browser = webdriver.Chrome()
+
+browser.get('http://www.baidu.com')
+time.sleep(0.3)
+print(browser.title)
+browser.find_element_by_id("kw").send_keys("selenium")
+browser.find_element_by_id("su").click()
+time.sleep(3)
+browser.quit()
